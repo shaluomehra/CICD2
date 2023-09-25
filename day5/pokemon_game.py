711713aa13c9691a3970ff0fd491c5462750dee7
Shaluo Mehra <smehra@spartaglobal.com>
2023-09-25 14:17:59 +0100

    pokemon game update

--- day5/pokemon_game.py
+++ day5/pokemon_game.py
@@ -1,12 +1,12 @@
 import json
 import requests
-
+# get the pokemon from the user
 def get_pokemon():
     pokemon = str(input("Enter a pokemon ").lower())
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}/")
     return json.loads(response.text)
 
-# Function to simulate a battle between two Pokémon
+# make a function to simulate a battle between the two Pokémon
 def battle(player_pokemon, player2_pokemon):
     player_attack = player_pokemon["stats"][1]["base_stat"]
     player2_attack = player2_pokemon["stats"][1]["base_stat"]
@@ -21,11 +21,10 @@ def battle(player_pokemon, player2_pokemon):
 player_pokemon = get_pokemon()
 player2_pokemon = get_pokemon()
 
-print(f"Player 1 chose {player_pokemon['name']}!")
-
 # Simulate the battle
 result = battle(player_pokemon, player2_pokemon)
 
+print(f"Player 1 chose {player_pokemon['name']}!")
 print(f"Player 2 chose {player2_pokemon['name']}!")
 print(f"The battle result is: {result}")
 
